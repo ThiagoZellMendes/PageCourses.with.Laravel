e3d25df6bda55644073c4f1a362ee10f1d3eb944
Thiago Mendes <thiagosmendes.th@gmail.com>
2020-07-15 15:37:11 -0300

    atualização dos cursos 15-07-2020

--- resources/views/site/cursos.blade.php
+++ resources/views/site/cursos.blade.php
@@ -31,10 +31,14 @@
 <section id="Banner" class="content-section">
     <div class="row featurette">
       <div class="col-md-7 order-md-2">
-        <h2 class="featurette-heading">Liguagem PHP<span class="text-muted">Progração</span></h2>
+        <h2 class="featurette-heading">Linguagem PHP<span class="text-muted">Progração</span></h2>
         <p class="lead">Aprenda as principais forma de programas com essa linguagem, e muito mais!</p>
+        <a class="btn btn-primary btn-xl" href="{{route('site.login')}}">Ir para o curso</a>
       </div>
       <div class="col-md-5 order-md-1">
+        <figure>
+          <img src="{{url('img/php-logo-w.png')}}" alt="Curso PHP">
+        </figure>
 
       </div>
     </div>
@@ -47,8 +51,12 @@
     <div class="col-md-7 order-md-2">
         <h2 class="featurette-heading">POSTGRESS<span class="text-muted">Banco de dados.</span></h2>
         <p class="lead">Se tranforme em ninja e manipulação de banco de dados com essa ferramenta!.</p>
-    </div>
+        <a class="btn btn-primary btn-xl" href="{{route('site.login')}}">Ir para o curso</a>
+      </div>
     <div class="col-md-5 order-md-1">
+      <figure>
+        <img src="{{url('img/maxresdefault.jpg')}}" alt="Curso PHP">
+      </figure>
 
     </div>
     </div>
@@ -61,8 +69,12 @@
       <div class="col-md-7 order-md-2">
         <h2 class="featurette-heading">Redes de computadores<span class="text-muted">Infraestrutura</span></h2>
         <p class="lead">Crie todos tipos de redes e aprenda como gerenciar todas as maquinas de sua rede.</p>
-    </div>
+        <a class="btn btn-primary btn-xl" href="{{route('site.login')}}">Ir para o curso</a>
+      </div>
     <div class="col-md-5 order-md-1">
+      <figure>
+        <img src="{{url('img/REDES.png')}}" alt="Curso PHP">
+      </figure>
 
     </div>
     </div>
@@ -75,8 +87,12 @@
       <div class="col-md-7 order-md-2">
         <h2 class="featurette-heading">Segurança da Informação<span class="text-muted">Segurança</span></h2>
         <p class="lead">Nesse curso você apredenderar as principais tecnicas para manter seus dandos em seguraça.</p>
-    </div>
+        <a class="btn btn-primary btn-xl" href="{{route('site.login')}}">Ir para o curso</a>
+      </div>
     <div class="col-md-5 order-md-1">
+      <figure>
+        <img src="{{url('img/seguranca.jpg')}}" alt="Curso PHP">
+      </figure>
 
 
     </div>
